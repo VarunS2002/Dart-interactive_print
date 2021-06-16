446756aad7471836281a79550c2ec6a3bedf0256
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-06-16 21:40:59 +0530

    Initialise empty Dart package using: dart create --template=package-simple

---
+++ interactive_print/example/interactive_print_example.dart
@@ -0,0 +1,6 @@
+import 'package:interactive_print/interactive_print.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ interactive_print/lib/interactive_print.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library interactive_print;
+
+export 'src/interactive_print_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
