57ee15a3ae4bd605bd72006f0f96ece662e3f03f
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-06-16 21:42:56 +0530

    Removed auto generated code and comments

--- interactive_print/example/interactive_print_example.dart
+++ interactive_print/example/interactive_print_example.dart
@@ -1,6 +1,3 @@
 import 'package:interactive_print/interactive_print.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- interactive_print/lib/interactive_print.dart
+++ interactive_print/lib/interactive_print.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library interactive_print;
 
 export 'src/interactive_print_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
