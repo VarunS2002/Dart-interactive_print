5c98aebc90c0f8ec7c77c0c99b2fe2c1152d8003
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-07-05 11:23:05 +0530

    Documented end user functions in interactive_print_base.dart

--- interactive_print/lib/src/interactive_print_base.dart
+++ interactive_print/lib/src/interactive_print_base.dart
@@ -4,16 +4,24 @@ import 'interactive_print_stub.dart'
     if (dart.library.io) 'interactive_print_native.dart'
     if (dart.library.js) 'interactive_print_js.dart' as ip;
 
+/// Captures calls to [print] in the [body] and forwards them to [writeln].
 void capturePrint(void Function() body) {
   runZoned(body, zoneSpecification: ZoneSpecification(print: (s, p, z, arg) {
     writeln(arg);
   }));
 }
 
+/// Writes [string] to the console without adding a newline.
+///
+/// Line may not be printed until a newline is added.
+/// Always add an empty \\n or [writeln] call after all [write] calls.
+/// May not work properly in debug builds, for use in release mode.
 void write(String string) {
   ip.write(string);
 }
 
+/// Writes [string] to the console and adds a newline if the string doesn't end
+/// in one.
 void writeln([String string = '']) {
   ip.write(string, newline: true);
 }
